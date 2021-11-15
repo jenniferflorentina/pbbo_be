30acf42dfee40d2602e4a01478e13b16e0e4df31
rvalessandro <ralessandro939@gmail.com>
2021-11-15 10:05:19 +0700

    Added hashing util

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.2
+	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
 	gopkg.in/dealancer/validate.v2 v2.1.0
 	gorm.io/driver/postgres v1.1.2
 	gorm.io/gorm v1.21.16
---
+++ hashing/hashing.go
@@ -0,0 +1,27 @@
+package hashing
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func hashAndSalt(pwd []byte) string {
+    hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.DefaultCost)
+    if err != nil {
+        log.Println(err)
+    }
+	
+    return string(hash)
+}
+
+func comparePasswords(hashedPwd string, plainPwd []byte) bool {
+    byteHash := []byte(hashedPwd)
+    err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+    if err != nil {
+        log.Println(err)
+        return false
+    }
+
+    return true
+}
