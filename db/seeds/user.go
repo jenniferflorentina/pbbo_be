6e786fbb094f885a0125540a8a8f0e6270acd445
rvalessandro <ralessandro939@gmail.com>
2021-11-21 15:33:58 +0800

    Added roles

--- db/seeds/user.go
+++ db/seeds/user.go
@@ -2,6 +2,7 @@ package seeds
 
 import (
 	"tubespbbo/db"
+	"tubespbbo/hashing"
 	"tubespbbo/modules/model"
 )
 
@@ -17,52 +18,52 @@ func (s *Seed) SeedUser() {
 	users := make([]model.User, 5)
 	users[0] = model.User{
 		Username: "Jojoan",
-		Password: "joan123",
+		Password: hashing.HashAndSalt([]byte("joan123")),
 		Name:     "Eirenika Joanna Grace",
 		Dob:      "2000-10-02",
 		Type:     0,
-		Address:   "Jln. Dimana-mana hatiku senang no.1",
-		Phone:  "081122334455",
+		Address:  "Jln. Dimana-mana hatiku senang no.1",
+		Phone:    "081122334455",
 		Email:    "joanjo@email.com",
 	}
 	users[1] = model.User{
 		Username: "JeniJenJen",
-		Password: "jeni123",
+		Password: hashing.HashAndSalt([]byte("jeni123")),
 		Name:     "Jenifer Florentina",
 		Dob:      "2001-06-18",
 		Type:     1,
-		Address:   "Jln. Dimana-mana hatiku gembira no.1",
-		Phone:  "081166887799",
+		Address:  "Jln. Dimana-mana hatiku gembira no.1",
+		Phone:    "081166887799",
 		Email:    "jeniakatina@email.com",
 	}
 	users[2] = model.User{
 		Username: "Marimar",
-		Password: "maria123",
+		Password: hashing.HashAndSalt([]byte("maria123")),
 		Name:     "Maria Vabiolla",
 		Dob:      "2001-10-12",
 		Type:     2,
-		Address:   "Jln. Dimana-mana hatiku bahagia no.1",
-		Phone:  "081100337788",
+		Address:  "Jln. Dimana-mana hatiku bahagia no.1",
+		Phone:    "081100337788",
 		Email:    "mariaAjah@email.com",
 	}
 	users[3] = model.User{
 		Username: "Enjeljel",
-		Password: "enjel123",
+		Password: hashing.HashAndSalt([]byte("enjel123")),
 		Name:     "Tridia Enjeliani",
 		Dob:      "2001-10-04",
 		Type:     3,
-		Address:   "Jln. Dimana-mana hatiku tertawa no.1",
-		Phone:  "081122998866",
+		Address:  "Jln. Dimana-mana hatiku tertawa no.1",
+		Phone:    "081122998866",
 		Email:    "jeniakatina@email.com",
 	}
 	users[4] = model.User{
 		Username: "Melpinz",
-		Password: "melvin123",
+		Password: hashing.HashAndSalt([]byte("melvin123")),
 		Name:     "Melvin Sebastian",
 		Dob:      "2001-07-21",
 		Type:     4,
-		Address:   "Jln. Dimana-mana hatiku hanya dia no.1",
-		Phone:  "0811229988",
+		Address:  "Jln. Dimana-mana hatiku hanya dia no.1",
+		Phone:    "0811229988",
 		Email:    "melpinzs@email.com",
 	}
 
